62bc0abea52ff898f6b2bcb6ffdd56a375e24a9e
panoplied <panoplied@protonmail.com>
2024-11-14 03:37:44 +0300

    Refactor opts

--- lua/options.lua
+++ lua/options.lua
@@ -1,76 +1,59 @@
 -- [[ OPTIONS ]]
 
--- NOTE: Must happen before plugins are loaded (otherwises wrong leader will be used)
-vim.g.mapleader = " "
+vim.g.mapleader = " "           -- NOTE: mapping leader must happen before loading plugins
 vim.g.maplocalleader = " "
 
 vim.opt.number = true
 vim.opt.mouse = 'a'
 
--- vim.opt.showmode = false        -- TODO: consider disabling showing mode when status line plugin installed
-
+vim.opt.showmode = false        -- TODO: consider disabling showing mode when status line plugin installed
 vim.opt.wrap = false
 
 -- Use OS clipboard
--- Schedule after `UiEnter` because it can increase startup time
-vim.schedule(function()
+vim.schedule(function()         -- Schedule after `UiEnter` because it can increase startup time
     vim.opt.clipboard = "unnamedplus"
 end)
 
 vim.opt.breakindent = true
-
--- Save undo history
 vim.opt.undofile = true
 
--- Case-insensitive searching unless \C or capital letters in the search term
-vim.opt.ignorecase = true
+vim.opt.ignorecase = true       -- Case-insensitive search unless \C or capital letters used
 vim.opt.smartcase = true
 
 vim.opt.signcolumn = "yes"
 
--- Decrease update time
-vim.opt.updatetime = 250
+vim.opt.updatetime = 250        -- Decrease update time
+vim.opt.timeoutlen = 300        -- Decrease mapped sequence wait time
 
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
--- TODO: enable when which-key popup set
--- vim.opt.timeoutlen = 300
+vim.opt.splitbelow = true       -- Vertical splits open below
+vim.opt.splitright = true       -- Horizontal splits open on the right
 
--- Open new vertical splits on the bottom, and horizontal on the right
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
--- Set up some whitespace chars display
-vim.opt.list = true
+vim.opt.list = true             -- Set up some whitespaces chars display (`h: 'list'`, `h: 'listchars'`)
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
-vim.opt.inccommand = "split"	-- Preview substitutions realtime in a split 
+vim.opt.inccommand = "split"    -- Preview substitutions realtime in a split
 
 vim.opt.cursorline = true
 
--- Allow positioning cursor where there is no actual character in Visual block mode
--- (really convenient for block selections)
-vim.opt.virtualedit = "block"
+vim.opt.scrolloff = 2           -- TODO: reconsider even having this
 
--- Support 24-bit RGB
-vim.opt.termguicolors = true
+vim.opt.virtualedit = "block"   -- Allow positioning cursor even where is no char (convenient for block selections <C-v>)
+
+vim.opt.termguicolors = true    -- Support 24-bit RGB
 
 
 -- [[ KEYMAPS ]]
 
--- Clear highlights on search when pressing <Esc> in normal mode
--- `:help hlsearch`
+-- Clear highlights on search when pressing <Esc> in normal mode (`:h hlsearch`)
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostics
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
--- Exit terminal mode with simpler shortcut (<Esc><Esc>)
--- NOTE: can be term emulator dependent, us <C-\><C-n> if broken
+-- Exit terminal mode with simpler shortcut (<Esc><Esc>) (NOTE: term dependent, use <C-\><C-n> if broken)
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- Split navigation with CTRL+[hjkl>]
--- `:help wincmd`
+-- Split navigation with CTRL+[hjkl] (`:h wincmd`)
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Set focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Set focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Set focus to the lower window" })
@@ -78,6 +61,9 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Set focus to the upper wind
 
 
 -- [[ AUTOCOMMANDS ]]
+-- (`:h lua-guide-autocommands`)
+
+-- Highlight when yanking (`:h vim.highlight.on_yank()`)
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight when yanking",
     group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
