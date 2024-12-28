086078e5c0fd457623922c9d6ec9d29f25f23519
panoplied <panoplied@protonmail.com>
2024-12-28 16:27:09 +0300

    Add couple colorscheme options, use kanagawa

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,14 @@
 return {
-  "folke/tokyonight.nvim",
+  "rebelot/kanagawa.nvim",
+  -- "folke/tokyonight.nvim",
+  -- "EdenEast/nightfox.nvim",
+  -- "catppuccin/nvim",
   lazy = false,
   priority = 1000,
   init = function()
-    vim.cmd.colorscheme("tokyonight-night")
+    vim.cmd.colorscheme("kanagawa-dragon")
+    -- vim.cmd.colorscheme("tokyonight-night")
+    -- vim.cmd.colorscheme("carbonfox")
+    -- vim.cmd.colorscheme("catppuccin-mocca")
   end,
 }
